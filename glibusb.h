b17aabd56fed67caaad55d44053f0e0adbe8c4ba
Andre Masella <andre@masella.name>
2012-09-11 19:27:09 -0700

    Fixed inclusion of glibusb.h

--- glibusb.h
+++ glibusb.h
@@ -1,3 +1,5 @@
+#ifndef GLIB_USB_H
+#define GLIB_USB_H
 #include <glib.h>
 #include <glib-object.h>
 #include <gio/gio.h>
@@ -14,3 +16,4 @@ enum libusb_transfer_status glibusb_interrupt_transfer_finish(GAsyncResult *resu
 
 void glibusb_bulk_transfer(libusb_device_handle *dev, unsigned char endpoint, unsigned int timeout, unsigned char *buffer, int buffer_length, GAsyncReadyCallback callback, gpointer user_data);
 enum libusb_transfer_status glibusb_bulk_transfer_finish(GAsyncResult *result, int *actual_length);
+#endif
